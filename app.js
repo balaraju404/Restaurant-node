6f6628084eeeef2389d97df35d4e2e86bbe59575
balaraju <balaraju@bijib.in>
2024-09-18 12:48:28 +0530

    socket io package

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ require('./config');
 require('./constants');
 const express = require('express');
 const cors = require('cors');
-const http = require('http');  // Add http to support WebSocket server
-const WebSocket = require('ws');
+const { Server } = require('socket.io');
+const http = require('http');
 const appRouter = require('./app/routes/index');
 
 const app = express();
@@ -32,35 +32,25 @@ app.use(express.json());
 
 app.use('/api', appRouter);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const appServer = http.createServer(app);
+const io = new Server(appServer, {
+  cors: {
+    origin: 'http://192.168.0.127:4299',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  }
+});
 
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
   console.log('New WebSocket client connected');
-
-  ws.on('message', (message) => {
-    console.log('Received message from client:', message);
-
-    // Example of broadcasting a properly formatted JSON message
-    const broadcastMessage = JSON.stringify({
-      type: 'NEW_ORDER',
-      data: message
-    });
-
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(broadcastMessage);
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    console.log('WebSocket client disconnected');
+  socket.on('orderData', (data) => {
+    io.emit('orderData', JSON.stringify(data));
   });
 });
 
 const PORT = process.env.PORT || 3099;
 
-server.listen(PORT, () => {
+appServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
